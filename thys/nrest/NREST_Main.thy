4ed2967217eb30618b2e166599f2fc8c7f1a5605
maxhaslbeck <haslbema@in.tum.de>
2020-06-11 09:01:37 +0200

    feat(NREST) setup for refine_rcg

--- thys/nrest/NREST_Main.thy
+++ thys/nrest/NREST_Main.thy
@@ -75,4 +75,143 @@ translations
 abbreviation RETURNTecost :: "'a \<Rightarrow> ('a, (string,enat) acost) nrest"
   where "RETURNTecost \<equiv> RETURNT"
 
+
+subsection \<open>Setup for refinement condition generator\<close>
+
+
+
+
+thm refine
+
+thm ASSERT_leI_f
+lemma ASSERT_D_leI[refine,refine0]: 
+  fixes M :: "(_,(_,enat)acost) nrest"
+  shows "\<Phi> \<Longrightarrow> (\<Phi> \<Longrightarrow> M \<le> \<Down>R M') \<Longrightarrow> ASSERT \<Phi> \<bind> (\<lambda>_. M) \<le> \<Down>R M'"
+  by (auto)
+
+lemma ASSERT_D2_leI[refine0]: 
+  fixes S' :: "(_,(_,enat)acost) nrest"
+  shows "(\<Phi> \<Longrightarrow> S \<le> \<Down> R S') \<Longrightarrow> S \<le> \<Down> R (do {
+                                     _ \<leftarrow> ASSERT \<Phi>;
+                                     S'
+                                   })"
+  by (auto simp: pw_acost_le_iff refine_pw_simps)
+
+
+lemma ASSERT_D3_leI[refine0]: 
+  fixes S' :: "(_,(_,enat)acost) nrest"
+  shows "(\<Phi> \<Longrightarrow> S \<le> \<Down> R (timerefine E S')) \<Longrightarrow> S \<le> \<Down> R (timerefine E (do {
+                                     _ \<leftarrow> ASSERT \<Phi>;
+                                     S'
+                                   }))"
+  by (auto simp: pw_acost_le_iff refine_pw_simps)
+
+
+lemma ASSERT_D4_leI[refine0]: 
+  fixes S' :: "(_,(_,enat)acost) nrest"
+  shows "(\<Phi> \<Longrightarrow> S \<le> \<Down> R (timerefine E S')) \<Longrightarrow> do { _ \<leftarrow> ASSERT \<Phi>; S } \<le> \<Down> R (timerefine E (do {
+                                     _ \<leftarrow> ASSERT \<Phi>;
+                                     S'
+                                   }))"
+  by (auto simp: pw_acost_le_iff refine_pw_simps)
+
+
+lemma refine_Id[refine0]: "S \<le> \<Down> Id S"
+  by auto
+
+
+definition "TId = (\<lambda>x. acostC (\<lambda>y. (if x=y then 1 else 0)))"
+
+lemma timerefine_id:
+  fixes M :: "(_,(_,enat)acost) nrest"
+  shows "timerefine TId M = M"
+  apply(cases M; auto simp: timerefine_def TId_def)
+  apply(rule ext) apply(auto split: option.splits)
+  subgoal for x2 r x2a apply(cases x2a) apply simp
+    apply(rule ext) apply(simp add: if_distrib)
+    apply(subst mult_zero_right)
+    apply(subst Sum_any.delta) by simp
+  done
+
+
+lemma refine_TId[refine0]:
+  fixes S :: "(_,(_,enat)acost) nrest"
+  shows  "S \<le> \<Down> Id (timerefine TId S)"
+  unfolding timerefine_id
+  by auto
+
+
+
+lemma the_acost_mono: "T \<le> T' \<Longrightarrow> the_acost T b \<le> the_acost T' b"
+  apply(cases T; cases T') by (auto simp: le_fun_def less_eq_acost_def)
+
+
+
+
+lemma SPEC_refine[refine]:
+  fixes T :: "_ \<Rightarrow> ((_,enat)acost)"
+  assumes *: "\<And>x. \<Phi> x \<Longrightarrow> \<exists>s'. \<Phi>' s' \<and> (x, s') \<in> R \<and> T x \<le> T' s'"
+  shows "SPEC \<Phi> T \<le> \<Down>R (SPEC \<Phi>' T')"
+  unfolding SPEC_def
+  by (auto simp: pw_acost_le_iff refine_pw_simps dest!: * intro: order.trans[OF _ the_acost_mono])
+
+
+
+lemma build_rel_SPEC_conv:
+  fixes T :: "_ \<Rightarrow> ((_,enat)acost)"
+  assumes "\<And>x. T (\<alpha> x) = T' x"
+  shows "\<Down>(br \<alpha> I) (SPEC \<Phi> T) = SPEC (\<lambda>x. I x \<and> \<Phi> (\<alpha> x)) T'"  
+  using assms by (auto simp: br_def pw_acost_eq_iff refine_pw_simps SPEC_def)
+
+lemma SPEC_cong: "\<Phi>=\<Phi>' \<Longrightarrow> T=T' \<Longrightarrow> SPEC \<Phi> T = SPEC \<Phi>' T'"
+  by simp
+
+
+
+lemma le_acost_ASSERTI:
+  fixes M :: "(_,(_,enat) acost) nrest"
+  shows "(\<Phi> \<Longrightarrow> M \<le> M') \<Longrightarrow> M \<le> ASSERT \<Phi> \<bind> (\<lambda>_. M')"
+  by(auto simp: pw_acost_le_iff refine_pw_simps)
+
+lemma consume_RETURNT: "consume (RETURNT x) T = SPECT [x \<mapsto> T]"
+  by(auto simp: RETURNT_def consume_def)
+
+  lemma gwp_specifies_acr_I: 
+    fixes m :: "(_,(_,enat) acost) nrest"
+    shows "(\<Phi> \<Longrightarrow> gwp m [x \<mapsto> T] \<ge> Some 0) \<Longrightarrow> (m \<le> doN { ASSERT \<Phi>; consume (RETURNT x) T })"
+    apply(rule le_acost_ASSERTI)
+    unfolding consume_RETURNT
+    apply(rule gwp_specifies_I) by simp
+thm refine
+thm refine0
+
+
+thm consume_mono[refine0]
+
+
+thm consume_mono[where M="M::(_,(_,enat)acost) nrest"]
+
+lemma consume_refine[refine0]:
+  fixes M :: "('e, ('b, enat) acost) nrest"
+  shows "\<lbrakk>t \<le> timerefineA E t'; M \<le> \<Down> R (timerefine E M')\<rbrakk> \<Longrightarrow> NREST.consume M t \<le> \<Down>R (timerefine E (NREST.consume M' t'))"
+  apply(subst timerefine_consume)
+  apply(subst conc_fun_consume)
+  apply(rule consume_mono) by auto
+
+lemma timerefine_RETURNT: "timerefine E (RETURNT x') = RETURNT x'"
+  by (auto simp: timerefine_def RETURNT_def zero_acost_def)
+
+lemma RETURNT_refine_t[refine]:
+  assumes "(x,x')\<in>R"
+  shows "RETURNT x \<le> \<Down>R (timerefine E (RETURNT x'))"
+  apply(subst timerefine_RETURNT)
+  apply(rule RETURNT_refine) by (fact assms)
+
+
+
+(* TODO: move *)
+
+
+
+
 end
