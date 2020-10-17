fbb801f857f943defd3bf2c76b3481488cbcabf4
Peter Lammich <plammich@gmail.com>
2020-10-17 13:56:11 +0100

    cleaned up, fixed ROOT, html generation

--- benchmarks/sorting/repeat_laptop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-echo -n '# '; date 
-echo -n '# '; uname -a 
-echo '# Repeated b/c difference > 15\% (1884,1516)'
-./do_benchmark uint64 std::sort random-boolean 100000000 10
-./do_benchmark uint64 isabelle::sort random-boolean 100000000 10
-echo '# Repeated b/c noise > 2\%'
-./do_benchmark uint64 isabelle::sort almost-sorted-10 100000000 10
--- mkdist.sh
+++ mkdist.sh
@@ -2,6 +2,18 @@
 
 DIR=${PWD##*/}
 
+if test ! -e thys.txt; then
+  echo Rebuilding theory list
+  isabelle build -n -D thys -d contrib/Imperative_HOL_Time -l | grep "^  $PWD/thys/" | sed -re "s|  $PWD/||" > thys.txt
+  echo Done
+fi
+
+
+rm -f dist.tar dist.tar.gz dist.tgz
+
 cd ..
-tar -czf "$DIR/dist.tgz" --exclude-ignore-recursive=.distignore --transform "s|^$DIR|isabelle-llvm|" "$DIR"
-cd - >/dev/null 
+tar -cf "$DIR/dist.tar" --exclude-ignore-recursive=.distignore --exclude="$DIR/thys/*" --exclude="$DIR/dist.tar" --transform "s|^$DIR|isabelle-llvm-time|" "$DIR"
+tar -rf "$DIR/dist.tar" -T <(sed -re "s|^|$DIR/|" $DIR/thys.txt) --transform "s|^$DIR|isabelle-llvm-time|"
+cd - >/dev/null
+gzip dist.tar
+mv dist.tar.gz dist.tgz
